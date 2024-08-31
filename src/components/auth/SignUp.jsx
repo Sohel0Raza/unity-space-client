3e997b87e80af83839b028cc358b171c4c7dede1
Sohel0Raza <soheldot1239@gmail.com>
2024-08-31 22:57:41 +0600

    create signup card style and validation email or phone

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -3,28 +3,53 @@ import { useHttp } from "../../hooks/useHttp";
 import { HTTP_METHOD } from "../../services/utils";
 import logo from "./../../../public/logo.png";
 import { HttpStatusCode } from "axios";
+import { useState } from "react";
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const [input, setInput] = useState("");
+  const [error, setError] = useState("");
+  const [inputType, setInputType] = useState(null);
 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const phoneRegex = /^(?:\+88|88)?01[3-9]\d{8}$/;
+
+  const checkInputType = (input) => {
+    if (emailRegex.test(input)) {
+      return "email";
+    } else if (phoneRegex.test(input)) {
+      return "phone";
+    } else {
+      return "invalid";
+    }
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setInput(value);
+
+    const type = checkInputType(value);
+    setInputType(type);
+
+    if (type === "invalid") {
+      setError("Please enter a valid email address or phone number.");
+    } else {
+      setError("");
+    }
+  };
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     const signupForm = event.target;
 
-    // const url = process.env.OTP_URL || "https://bulksmsbd.net/api/smsapi";
-    // const ApiKey = process.env.OTP_API_KEY ?? "9c2A8z1cAuUcAuBi3ito";
-    // const SenderId = process.env.OTP_SENDER_ID ?? "8809617618737";
+    const url = import.meta.env.OTP_URL;
+    const ApiKey = import.meta.env.OTP_API_KEY;
+    const SenderId = import.meta.env.OTP_SENDER_ID;
 
-    const url = "https://bulksmsbd.net/api/smsapi";
-    const ApiKey = "9c2A8z1cAuUcAuBi3ito";
-    const SenderId = "8809617618737";
-
-    const phoneNumber = signupForm.phoneOrEmail.value;
+    const phoneNumber = input;
 
     const randomNum = Math.random() * 9000;
     const otp = Math.floor(1000 + randomNum);
-    console.log("✌️otp --->", otp);
 
     const content = {
       api_key: ApiKey,
@@ -43,15 +68,13 @@ const SignUp = () => {
     )
       return;
 
-    
-
     const user = {
       firstName: signupForm.firstName.value,
       lastName: signupForm.lastName.value,
       phoneOrEmail: phoneNumber,
       password: signupForm.password.value,
       gender: "male",
-      otp: otp
+      otp: otp,
     };
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -69,7 +92,7 @@ const SignUp = () => {
   };
 
   return (
-    <div className="bg-[#F2F4F7] min-h-screen">
+    <div className="bg-[#F2F4F7] min-h-screen ">
       <div>
         <div className="w-[300px] mx-auto flex justify-center items-center py-5">
           <div className="h-20 w-20 -ml-5">
@@ -84,7 +107,7 @@ const SignUp = () => {
         </div>
         <div className="hero">
           <div className="hero-content bg-[#FFFFFF] shadow-2xl w-full md:w-2/6 flex-col lg:flex-row-reverse rounded">
-            <div className="card w-full">
+            <div className="card w-full relative">
               <div className="pl-9 pt-2">
                 <h2 className="text-2xl font-bold text-[#2d545e]">Sign Up</h2>
                 <p>It is esay and secure</p>
@@ -117,7 +140,16 @@ const SignUp = () => {
                     name="phoneOrEmail"
                     placeholder="Mobile number or email address"
                     className="myInput"
+                    value={input}
+                    onChange={handleChange}
                   />
+                  {error && (
+                    <p className="text-white text-sm p-2 mt-2 md:mt-0
+                     bg-[#6e2c2c] md:absolute md:top-[163px] md:-left-[320px] ">
+
+                      {error}
+                    </p>
+                  )}
                 </div>
                 <div className="form-control">
                   <input
